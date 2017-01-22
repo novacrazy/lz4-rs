a522a5ed21ebf1e4c6c13db2105c2125e6e62197
Aaron Trent <novacrazy@gmail.com>
2017-01-22 02:21:26 -0600

    Formatting

--- lz4-sys/build.rs
+++ lz4-sys/build.rs
@@ -4,6 +4,7 @@ use std::env;
 
 fn main() {
     let mut compiler = gcc::Config::new();
+
     compiler
         .file("liblz4/lib/lz4.c")
         .file("liblz4/lib/lz4frame.c")
@@ -11,13 +12,13 @@ fn main() {
         .file("liblz4/lib/xxhash.c")
         // We always compile the C with optimization, because otherwise it is 20x slower.
         .opt_level(3);
-    match env::var("TARGET").unwrap().as_str()
-    {
-      "i686-pc-windows-gnu" => {
-        compiler
-            .flag("-fno-tree-vectorize");
-      },
-      _ => {}
+
+    match env::var("TARGET").unwrap().as_str() {
+        "i686-pc-windows-gnu" => {
+            compiler.flag("-fno-tree-vectorize");
+        },
+        _ => {}
     }
+
     compiler.compile("liblz4.a");
 }
